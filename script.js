adb9fea3e032b1d54f97bc153ee6480a44792acd
seamstress-studio <gaelboulard@gmail.com>
2025-02-03 17:07:28 +0100

    Update script.js
    plusieurs pages

--- script.js
+++ script.js
@@ -27,3 +27,14 @@ setInterval(function() {
     score += autoClickers * 5;
     updateScore();
 }, 1000);
+
+function showPage(pageId) {
+    document.querySelectorAll(".page").forEach(page => {
+        page.style.display = "none";
+    });
+    document.getElementById(pageId).style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    showPage("actions");
+});
